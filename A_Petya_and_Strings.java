6b5e22c88f2dda5b50313bac6923aefc0f493959
aayanksinghai <aayanksinghai02@gmail.com>
2025-08-15 19:50:23 +0530

    A Petya and Strings

---
+++ A_Petya_and_Strings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class A_Petya_and_Strings {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        
+        // Direct Function in Java
+        int result = str1.compareToIgnoreCase(str2);
+        if(result < 0){
+            System.out.println(-1);
+        }else if(result > 0) System.out.println(1);
+        else System.out.println(0);
+
+        sc.close();
+    }
+}
